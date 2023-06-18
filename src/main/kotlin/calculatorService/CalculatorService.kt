465f68028c7403c9ff02d9df173605568a00c56e
cloudwi <cloudwi@naver.com>
2023-06-18 20:49:18 +0900

    feat : 연산을 실제로 담당하는 서비스 클래스 분리

---
+++ src/main/kotlin/calculatorService/CalculatorService.kt
@@ -0,0 +1,12 @@
+package calculatorService
+
+class CalculatorService {
+
+    fun plus(number1: Int, number2: Int): Int {
+        return number1 + number2
+    }
+
+    fun subtract(number1: Int, number2: Int): Int {
+        return number1 - number2
+    }
+}
